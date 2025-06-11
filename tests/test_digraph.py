aaba0598dc3c6679d81a8e45dcaea1e0fea2f7da
Saurabh Misra <misra.saurabh1@gmail.com>
2025-06-11 00:54:43 -0700

    try an idea

--- tests/test_digraph.py
+++ tests/test_digraph.py
@@ -1,11 +1,9 @@
-import pytest
-
 import networkx as nx
+import pytest
 from networkx.utils import nodes_equal
-
-from .test_graph import BaseAttrGraphTester, BaseGraphTester
-from .test_graph import TestEdgeSubgraph as _TestGraphEdgeSubgraph
-from .test_graph import TestGraph as _TestGraph
+from test_graph import BaseAttrGraphTester, BaseGraphTester
+from test_graph import TestEdgeSubgraph as _TestGraphEdgeSubgraph
+from test_graph import TestGraph as _TestGraph
 
 
 class BaseDiGraphTester(BaseGraphTester):
@@ -161,14 +159,7 @@ class BaseDiGraphTester(BaseGraphTester):
 class BaseAttrDiGraphTester(BaseDiGraphTester, BaseAttrGraphTester):
     def test_edges_data(self):
         G = self.K3
-        all_edges = [
-            (0, 1, {}),
-            (0, 2, {}),
-            (1, 0, {}),
-            (1, 2, {}),
-            (2, 0, {}),
-            (2, 1, {}),
-        ]
+        all_edges = [(0, 1, {}), (0, 2, {}), (1, 0, {}), (1, 2, {}), (2, 0, {}), (2, 1, {})]
         assert sorted(G.edges(data=True)) == all_edges
         assert sorted(G.edges(0, data=True)) == all_edges[:2]
         assert sorted(G.edges([0, 1], data=True)) == all_edges[:4]
